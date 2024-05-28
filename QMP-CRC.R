6dac7c1fff31037b5ebfba9318d080eef716a567
Shadman Ishraq <shadmanishraq2@gmail.com.com>
2024-05-28 10:09:46 +0300

    modification of code

--- QMP-CRC.R
+++ QMP-CRC.R
@@ -1,28 +1,49 @@
-# Load the mia R package
+# Load necessary packages
 library(mia)
+library(stringr)
+library(S4Vectors)
+
+### Set Working Directory ###
+setwd("C:/Users/shadm/Desktop/CRC_QMP_example_data")
 
 ### Defining FILE PATHS ###
-
-url_to_assay <- url("https://raw.githubusercontent.com/ShadmanIshraq/CRC_QMP_example_data/main/transposed_CRC_assay.csv")
-url_to_coldata <- url("https://raw.githubusercontent.com/ShadmanIshraq/CRC_QMP_example_data/main/CRC_coldata.csv")
-url_to_rowdata <- url("https://raw.githubusercontent.com/ShadmanIshraq/CRC_QMP_example_data/main/CRC_rowdata.csv")
-
+path_to_assay <- "CRC_assay.csv"
+path_to_coldata <- "CRC_coldata.csv"
+path_to_rowdata <- "CRC_rowdata.csv"
 
 ### Importing FILES ###
-
-CRC_assay <- read.csv(url_to_assay,row.names = 1)
+CRC_assay <- read.csv(path_to_assay, row.names = 1, check.names = FALSE)
 colnames(CRC_assay) <- gsub("\\.", "-", colnames(CRC_assay))
 
-CRC_coldata <- read.csv(url_to_coldata, row.names = 1)
-CRC_rowdata <- read.csv(url_to_rowdata, row.names = 1)
+CRC_coldata <- read.csv(path_to_coldata, row.names = 1)
+CRC_rowdata <- read.csv(path_to_rowdata, row.names = 1)
+
+# Replace periods with underscores in rowdata column names
+colnames(CRC_rowdata) <- str_replace_all(colnames(CRC_rowdata), "\\.", "_")
+
+# Convert empty strings to NA in coldata and rowdata
+CRC_coldata[CRC_coldata == ""] <- NA
+CRC_rowdata[CRC_rowdata == ""] <- NA
+
+# Convert CRC_coldata and CRC_rowdata to DataFrame objects
+CRC_coldata <- DataFrame(CRC_coldata)
+CRC_rowdata <- DataFrame(CRC_rowdata)
+
+# Convert colData fields to factors
+CRC_coldata[] <- lapply(CRC_coldata, function(x) {
+  if (is.character(x)) {
+    return(as.factor(x))
+  } else {
+    return(x)
+  }
+})
 
 ### Constructing TSE ###
-
-tse <- TreeSummarizedExperiment(assays = list(counts = CRC_assay),
-                                colData = CRC_coldata,
-                                rowData = CRC_rowdata)
+tse <- TreeSummarizedExperiment(
+  assays = SimpleList(counts = as.matrix(CRC_assay)),
+  colData = CRC_coldata,
+  rowData = CRC_rowdata
+)
 
 ### SAVE into RDS ###
 saveRDS(tse, file = "CRCtse.rds")
-
-
